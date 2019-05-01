dce934710839b53b4d598896fd78d9cb3e3a77f6
nicor88 <nicor88@users.noreply.github.com>
2019-05-01 13:53:21 +0200

    add pandas to test

--- lambdas/hello_world/src/lambda_function.py
+++ lambdas/hello_world/src/lambda_function.py
@@ -1,7 +1,9 @@
+import pandas
 import requests
 
 
 def lambda_handler(event, context):
     print('Hello World')
     print(requests.__version__)
+    print(pandas.__version__)
     return {'output': 'hello world'}
